365475bc0e8cb9f69b166e063e7db9dc22b61592
Darcy DeClute <96313181+scrumtuous@users.noreply.github.com>
2026-08-27 06:16:30 -0700

    Add comment with image URL in BuildMessage.java
    Added a comment with a URL related to the build message.

--- src/main/java/com/mcnz/maven/example/BuildMessage.java
+++ src/main/java/com/mcnz/maven/example/BuildMessage.java
@@ -1,10 +1,13 @@
 package com.mcnz.maven.example;
 
 public class BuildMessage {
-	
 	public static String getInspired() {
 		String s = "Build all the things!";
 		return s;
 	}
 
 }
+
+/*
+https://www.theserverside.com/assets/recovered/build-all-the-things.webp
+*/
